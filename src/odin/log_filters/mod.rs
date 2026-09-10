d10489f240265c9b2371bc11a51edac79eccf4ed
ksc98 <kylechang96@gmail.com>
2026-09-10 00:00:02 -0700

    odin+huginn: world stats from the server log (objects, day, load time, save durations, rpc timeouts, wrong passwords)

--- src/odin/log_filters/mod.rs
+++ src/odin/log_filters/mod.rs
@@ -1,7 +1,9 @@
 mod player;
 mod probes;
 mod save;
+mod world;
 
 pub use player::{handle_player_events, OnlinePlayer, PlayerList};
 pub use probes::handle_launch_probes;
 pub use save::handle_save_events;
+pub use world::{handle_world_events, Save, WorldStats, WrongPassword};
